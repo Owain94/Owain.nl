7dd8f0cdaa67a6e0c8563bd7f3d933230318db83
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-03-04 08:47:51 +0100

    Use a for loop for skills

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  public skills = [
+    { name: 'Angular', color: 'primary' },
+    { name: 'AngularJS', color: 'accent' },
+    { name: 'TypeScript', color: 'primary' },
+    { name: 'JavaScript', color: 'primary' },
+    { name: 'NodeJS', color: 'none' },
+    { name: 'ASP.NET / ASP.NET MVC', color: 'primary' },
+    { name: 'Entity Framework', color: 'accent' },
+    { name: 'WinForms', color: 'accent' },
+    { name: 'ADO.NET', color: 'accent' },
+    { name: 'C#', color: 'primary' },
+    { name: 'HTML', color: 'primary' },
+    { name: 'CSS', color: 'primary' },
+    { name: 'Java', color: 'none' },
+    { name: 'PHP', color: 'primary' },
+    { name: 'Python', color: 'accent' },
+    { name: 'MySQL / PostgreSQL', color: 'accent' }
+  ];
 }
