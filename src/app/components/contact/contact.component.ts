71abed37bcda004fd304ea22bc73e12491355faf
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-03-05 11:37:40 +0100

    Only catch the most obvious syntax errors, validating is impossible anyways

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -30,8 +30,7 @@ export class ContactComponent {
   }
 
   private validateEmail(control: FormControl) {
-    // tslint:disable-next-line:max-line-length
-    const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const EMAIL_REGEXP = /\S+@\S+\.\S+/;
 
     return EMAIL_REGEXP.test(control.value) ? null : {
       validateEmail: {
