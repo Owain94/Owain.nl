8a9d616da668166386b907c4534a3cb8c3b2da4f
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-03-05 08:45:50 +0100

    Useless cat. Consider 'cmd < file | ..' or 'cmd file | ..' instead.

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    cat dist/index.html | gsed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' | gsed 's/\(.*\)<link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | gsed -e 's:</script>:</script>\n:g' | gsed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
+    gsed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' < dist/index.html | gsed 's/\(.*\)<link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | gsed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | gsed -e 's:</script>:</script>\n:g' | gsed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
     mv -f dist/index2.html dist/index.html
 else
-   cat dist/index.html | sed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' | sed 's/\(.*\)<link\(.*\)/\1  <link\2/' | sed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | sed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | sed -e 's:</script>:</script>\n:g' | sed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
+   sed 's/\(.*\)><\/s\(.*\)/\1 async><\/s\2/' < dist/index.html | sed 's/\(.*\)<link\(.*\)/\1  <link\2/' | sed 's/\(.*\)    <link\(.*\)/\1  <link\2/' | sed 's/\(.*\)<\/head>\(.*\)/\1\n<\/head>\2/' | sed -e 's:</script>:</script>\n:g' | sed 's/\(.*\)<script\(.*\)/\1  <script\2/' > dist/index2.html;
     mv -f dist/index2.html dist/index.html
 fi
