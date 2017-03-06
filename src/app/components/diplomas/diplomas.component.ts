91ce403508935c44f03af9c446f07b3a1c3833a9
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-03-06 18:08:17 +0100

    Let angular render this

--- src/app/components/diplomas/diplomas.component.ts
+++ src/app/components/diplomas/diplomas.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./diplomas.component.scss']
 })
 export class DiplomasComponent {
-
+  public diplomas =
+  [
+    [
+      { name: 'Mediadeveloper', image: 'glr', date: 'juni 2015' },
+      { name: 'Microsoft Technology Associate', image: 'microsoft', date: 'juli 2014' }
+    ],
+    [
+      { name: 'Microsoft Certified Professional', image: 'microsoft', date: 'juli 2014' },
+      { name: 'Basisveiligheid VCA', image: 'intron', date: 'februari 2011 – februari 2021' }
+    ]
+  ];
 }
