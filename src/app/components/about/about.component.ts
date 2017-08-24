aaf84e1e2e7a4f9785051a824431fdcad110c89e
Owain van Brakel <owain.vanbrakel@gmail.com>
2017-08-24 12:46:04 +0200

    Add links to the about component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -11,22 +11,23 @@ import { Log } from '../../decorators/log.decorator';
 @Log()
 export class AboutComponent {
   public skills = [
-    { name: 'Angular', color: 'primary' },
-    { name: 'AngularJS', color: 'accent' },
-    { name: 'TypeScript', color: 'primary' },
-    { name: 'JavaScript', color: 'primary' },
-    { name: 'NodeJS', color: 'none' },
-    { name: 'VueJS', color: 'none' },
-    { name: 'ASP.NET / ASP.NET MVC', color: 'primary' },
-    { name: 'Entity Framework', color: 'accent' },
-    { name: 'WinForms', color: 'accent' },
-    { name: 'ADO.NET', color: 'accent' },
-    { name: 'C#', color: 'primary' },
-    { name: 'HTML', color: 'primary' },
-    { name: 'CSS', color: 'primary' },
-    { name: 'Java', color: 'none' },
-    { name: 'PHP', color: 'primary' },
-    { name: 'Python', color: 'accent' },
-    { name: 'MySQL / PostgreSQL', color: 'accent' }
+    { name: 'Angular', color: 'primary', link: 'https://angular.io/' },
+    { name: 'AngularJS', color: 'accent', link: 'https://angularjs.org/' },
+    { name: 'TypeScript', color: 'primary', link: 'https://www.typescriptlang.org/' },
+    { name: 'JavaScript', color: 'primary', link: 'https://www.javascript.com/' },
+    { name: 'NodeJS', color: 'none', link: 'https://nodejs.org/en/' },
+    { name: 'VueJS', color: 'none', link: 'https://vuejs.org/' },
+    { name: 'ASP.NET / ASP.NET MVC', color: 'primary', link: 'https://www.asp.net/mvc' },
+    { name: 'Entity Framework', color: 'accent', link: 'https://msdn.microsoft.com/en-us/library/aa937723(v=vs.113).aspx' },
+    { name: 'WinForms', color: 'accent', link: 'https://docs.microsoft.com/en-us/dotnet/framework/winforms/' },
+    { name: 'ADO.NET', color: 'accent', link: 'https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/ado-net-overview' },
+    { name: 'C#', color: 'primary', link: 'https://docs.microsoft.com/en-us/dotnet/csharp/csharp' },
+    { name: 'HTML', color: 'primary', link: 'https://www.w3.org/html/' },
+    { name: 'CSS', color: 'primary', link: 'https://www.w3.org/Style/CSS/' },
+    { name: 'Java', color: 'none', link: 'https://www.java.com/en/' },
+    { name: 'PHP', color: 'primary', link: 'https://secure.php.net/' },
+    { name: 'Python', color: 'accent', link: 'https://www.python.org/' },
+    { name: 'MySQL', color: 'accent', link: 'https://www.mysql.com/' },
+    { name: 'PostgreSQL', color: 'accent', link: 'https://www.postgresql.org/' }
   ];
 }
